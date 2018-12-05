82a9728e6a1b0df17adde9c6725fbbf481d8b1e3
Przemyslaw Wojcik <Przemyslaw.Wojcik@cshark.com>
2018-12-05 18:36:32 +0100

    Add project files.

---
+++ Identity/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Identity.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddMvc();
+
+            services.AddIdentityServer()
+                    .AddDeveloperSigningCredential()
+                    .AddInMemoryApiResources(InMemoryConfiguration.ApiResources())
+                    .AddInMemoryClients(InMemoryConfiguration.Clients())
+                    .AddTestUsers(InMemoryConfiguration.Users().ToList());
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+
+            app.UseCors(builder =>
+            {
+                builder.WithOrigins("*");
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+            });
+
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
---
+++ OcelotIdentity/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+
+namespace OcelotIdentity
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+
+            var authenticationProviderKey = "TestKey";
+
+            services.AddOcelot(Configuration).AddConsul();
+
+            services.AddAuthentication()
+                .AddIdentityServerAuthentication(authenticationProviderKey, options =>
+                {
+                    options.Authority = "http://localhost:7060/";
+                    options.ApiName = "socialnetwork";
+                    options.SupportedTokens = SupportedTokens.Jwt;
+                    options.ApiSecret = "secret";
+                    options.RequireHttpsMetadata = false;
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(builder =>
+            {
+                builder.WithOrigins("*");
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+            });
+
+            app.UseOcelot().Wait();
+        }
+    }
+}
