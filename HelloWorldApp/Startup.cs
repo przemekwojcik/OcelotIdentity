648c52cc85af25fa82f57801f3b92e85b167a6dc
Przemyslaw Wojcik <Przemyslaw.Wojcik@cshark.com>
2018-12-17 14:02:36 +0100

    changes

--- HelloWorldApp/Startup.cs
+++ HelloWorldApp/Startup.cs
@@ -2,45 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
-using Ocelot.Provider.Consul;
 
-namespace OcelotIdentity
+namespace HelloWorldApp
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
-
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
-            services.AddOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseCors(builder =>
+            if (env.IsDevelopment())
             {
-                builder.WithOrigins("*");
-                builder.AllowAnyHeader();
-                builder.AllowAnyMethod();
-            });
+                app.UseDeveloperExceptionPage();
+            }
 
-            await app.UseOcelot();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -6,24 +6,48 @@ using Identity.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Logging;
 
 namespace Identity
 {
     public class Startup
     {
+
+        public IHostingEnvironment Environment { get; }
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+            IdentityModelEventSource.ShowPII = true;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                          b => b.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .AllowCredentials());
+            });
+            var host = Configuration.GetSection("Hosts")?.GetSection("Externals")?.GetSection("CurrentUri")?.Value;
             services.AddIdentityServer()
                     .AddDeveloperSigningCredential()
                     .AddInMemoryApiResources(InMemoryConfiguration.ApiResources())
                     .AddInMemoryClients(InMemoryConfiguration.Clients())
-                    .AddTestUsers(InMemoryConfiguration.Users().ToList());
+                    .AddTestUsers(InMemoryConfiguration.Users().ToList())
+                    .AddJwtBearerClientAuthentication();
 
         }
 
@@ -35,17 +59,15 @@ namespace Identity
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseIdentityServer();
 
-            app.UseCors(builder =>
+            if (!Environment.IsDevelopment())
             {
-                builder.WithOrigins("*");
-                builder.AllowAnyHeader();
-                builder.AllowAnyMethod();
-            });
+                app.UseForwardedHeaders();
+            }
 
+            app.UseCors("CorsPolicy");
+            app.UseIdentityServer();
             app.UseStaticFiles();
-
             app.UseMvcWithDefaultRoute();
         }
     }
